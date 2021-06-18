75599d606927980dee9ea93d63cf4ad14977b5fe
dpnbb <450030469@qq.com>
2021-06-18 13:47:52 +0800

    增加介绍

--- server/msg.h
+++ server/msg.h
@@ -1,15 +1,32 @@
 /**
- * Login        ：登录
- * Register     ：注册
- * SendToUser   ：向用户发送消息
- * SentToGroup  ：向群组发送消息
+ * Login                ：登录
+ * Register             ：注册
+ * SendToUser           ：向用户发送消息
+ * SentToGroup          ：向群组发送消息
+ * QuereHistoryFromUser ：查找与用户的历史消息
+ * QuereHistoryFromGroup：查找群聊历史消息
+ * AddGroup             ：建群
+ * DeleteGroup          ：删群
+ * JoinGroup            ：加群
+ * QuitGroup            ：退群
  */
-enum Type { Login, Register, SendToUser, SendToGroup };
+enum Type {
+    Login,
+    Register,
+    SendToUser,
+    SendToGroup,
+    QuereHistoryFromUser,
+    QuereHistoryFromGroup,
+    AddGroup,
+    DeleteGroup,
+    JoinGroup,
+    QuitGroup,
+};
 
 /**
  * command  ： 确定消息类型
- * target   ： 目标（若为向用户发送消息，目标为用户名
- *                  若为向群组发送消息，目标为群组名
+ * target   ： 目标（若为向用户发送消息或查找用户历史消息，目标为用户名
+ *                  若为向群组发送消息或查找群组历史消息，抑或是加群和退群，目标为群组名
  *                  其他可填server，也可不填此选项）
  * me       ： 用户自身
  * buf      ： 内容（若为登录或者注册，则内容填密码）
@@ -22,4 +39,4 @@ typedef struct Msg {
     char me[20];
     unsigned buflen;
     char buf[2048];
-}msg;
+} msg;
