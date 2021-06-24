98f68114a6143b2463b963ea087b665d1b5fca57
dpnbb <450030469@qq.com>
2021-06-24 12:43:53 +0800

    server主函数(epoll)

---
+++ server/server.h
@@ -0,0 +1,24 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "msghandle.h"
+
+#define IPADDRESS INADDR_ANY
+#define PORT 3456
+//#define MAXSIZE 2048
+#define LISTENQ 5
+#define FDSIZE 1000
+#define EPOLLEVENTS 100
+
+#endif
